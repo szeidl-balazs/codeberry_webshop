625112a2d5ae5d9367b19a8428a637fa746fd5b4
bszeidl <szeidl.balazs@hotmail.com>
2023-02-13 23:05:07 +0100

    The code has been refactored to class pattern, the first product object is ready, the second to be done.

--- module.js
+++ module.js
@@ -1,80 +1,95 @@
 //Structure module selects html tags and have the methods to modify the content of html elements:
 
-const Structure = function () {
+class shopShelf {
 
-    //private variables
+   
     //store elements selected by classes
-    this.productImageElement = document.querySelector(".js-product-image");
-    this.productNameElement = document.querySelector(".js-product-name");
-    this.productCategoryElement = document.querySelector(".js-product-category");
-    this.productDescriptionElement = document.querySelector(".js-product-description");
-    this.productPriceElement = document.querySelector(".js-product-price");
+    //selected based on the node (html element)
+    //to which the selected elements appended as child
     
+    constructor (node) {
+        this.node = node;
+        this.productImageElement = this.node.querySelector(".js-product-image");
+        this.productNameElement = this.node.querySelector(".js-product-name");
+        this.productCategoryElement = this.node.querySelector(".js-product-category");
+        this.productDescriptionElement = this.node.querySelector(".js-product-description");
+        this.productPriceElement = this.node.querySelector(".js-product-price");            
+    }
+
+    setProductImage (src) {
+        this.productImageElement.src = src;
+    };
+
+    setProductName (name) {
+        this.productNameElement.textContent = name;
+    };
+
+
+    setProductCategory (category) {
+        this.productCategoryElement.textContent = category;
+    };
+
+    setProductDescription (description) {
+        this.productDescriptionElement.textContent = description;
+    };
+
+    setProductPrice (price) {
+        this.productPriceElement.textContent = price;
+    };
 }; 
 
-Structure.prototype.setProductImage = function (src) {
-    this.productImageElement.src = src;
-};
-
-Structure.prototype.setProductName = function (name) {
-    this.productNameElement.textContent = name;
-};
-
-Structure.prototype.setProductCategory = function (category) {
-    this.productCategoryElement.textContent = category;
-};
-
-Structure.prototype.setProductDescription = function (description) {
-    this.productDescriptionElement.textContent = description;
-};
-
-Structure.prototype.setProductPrice = function (price) {
-    this.productPriceElement.textContent = price;
-};
 
 //Product objects which property values are the html element contents and methodes to return them:
 
-const Content = function () {
+class Product {
 
-    //private variables:
+    constructor (productInfo) {
+        
+        this.img = productInfo.img;
+        this.name = productInfo.name;
+        this.category = productInfo.category;
+        this.description = productInfo.description;
+        this.price = productInfo.price;
 
-    this.img = "images/discatcher.jpg";
-    this.name = "DisCatcher Target";
-    this.category = "Discgolf";
-    this.description = "a chain grid that catches fast and slow putts, heavy and light discs like no other target";
-    this.price = 399;
+    }
+
+    getImage () {
+        return this.img;
+    }
+
+    getName () {
+        return this.name;
+    }
+
+    getCategory () {
+        return this.category;
+    }
+
+    getDescription () {
+        return `${this.name} is ${this.description}.`;
+    }
+
+    getPrice = function () {
+        return `${this.price}EUR` ;
+    }
         
 };
 
-Content.prototype.getImage = function () {
-    return this.img;
-};
-
-Content.prototype.getName = function () {
-    return this.name;
-};
-
-Content.prototype.getCategory = function () {
-    return this.category;
-};
-
-Content.prototype.getDescription = function () {
-    return `${this.name} is ${this.description}.`;
-},
-
-Content.prototype.getPrice = function () {
-    return `${this.price}EUR` ;
-}
-
-const DisCatcher = new Content();
 
 
-const ShopStructure = new Structure();
+const discgolfProduct = new Product ({
+    img: "images/discatcher.jpg",
+    name: "DisCatcher Target",
+    category: "Discgolf",
+    description: "a chain grid that catches fast and slow putts, heavy and light discs like no other target",
+    price: 399
+});
 
-// A html elemeket módosító setProductX metódusokat meg kell hívni
-// a Product object értékeivel, amik akkor érvényesülnek, ha őket is meghívjuk a getX() metódusokkal.
-ShopStructure.setProductImage(DisCatcher.getImage());
-ShopStructure.setProductName(DisCatcher.getName());
-ShopStructure.setProductCategory(DisCatcher.getCategory());
-ShopStructure.setProductDescription(DisCatcher.getDescription());
-ShopStructure.setProductPrice(DisCatcher.getPrice());
+const shopShelf1 = new shopShelf (document.querySelector(".shop-shelf"));
+
+shopShelf1.setProductImage(discgolfProduct.getImage());
+shopShelf1.setProductName(discgolfProduct.getName());
+shopShelf1.setProductCategory(discgolfProduct.getCategory());
+shopShelf1.setProductDescription(discgolfProduct.getDescription());
+shopShelf1.setProductPrice(discgolfProduct.getPrice());
+
